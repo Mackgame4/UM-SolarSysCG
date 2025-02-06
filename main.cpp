b25530d82baae786ca6d63286578f1e1898b1587
Mack <42896437+Mackgame4@users.noreply.github.com>
2025-02-06 00:15:04 +0000

    Update some syntax

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
-#define _USE_MATH_DEFINES
-#include <math.h>
-#include <stdio.h>
+#include "globals.h"
 #include "config.h"
 
 // Global time variable
